326be62116416d45d7e75b22bd8b33bcf7d38d1c
Nathalie DE LA BURGADE <nade-la-@e2r8p11.clusters.42paris.fr>
2021-12-28 19:59:23 +0100

    pasgchose

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,32 +6,33 @@
 /*   By: nade-la- <nade-la-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 15:43:13 by nade-la-          #+#    #+#             */
-/*   Updated: 2021/12/22 19:26:26 by nade-la-         ###   ########.fr       */
+/*   Updated: 2021/12/28 19:55:43 by nade-la-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+int	check_set(const char *set, int c)
 {
-	while (*str && *str != (char)c)
-		str++;
-	if (*str == (char)c)
-		return ((char *)str); // faire une fonction pour checker set 
-	return (NULL);// caractere par caractere avec booleen / indice ...
-}	// start && end
+	while (*set && *set != (char)c)
+		set++;
+	if (*set == (char)c)
+		return (1);
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	char	c;
 	char	*trimd;
-	size_t	len;
 
 	if (!s1 || !set)
 		return (NULL);
-	while (s1 && ft_strchr(s1, set))
+	c = 0;
+	while (s1 && check_set(set, c) != 1)
 		s1++;
 	len = ft_strlen(s1);
-	while (len-- > 0 && ft_strchr(s1[len]))
+	while (len-- > 0 && check_set(set, c) != 1)
 	{
 		len = ft_strlen(s1);
 		trimd = malloc(sizeof(char) * (len + 1));
