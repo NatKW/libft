f30f1225d9bdc42ac0017d3003bbc309a226b9e2
NatKW <92329992+NatKW@users.noreply.github.com>
2022-01-18 20:49:51 +0100

    itoafinimaismarchepas

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nade-la- <nade-la-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thnab <thnab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 19:22:32 by nade-la-          #+#    #+#             */
-/*   Updated: 2022/01/15 19:41:28 by nade-la-         ###   ########.fr       */
+/*   Updated: 2022/01/18 20:46:05 by thnab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_lennbr(int n)
+static int	ft_lennbr(long int n)
 {	
 	int	len;
 
@@ -21,6 +21,7 @@ static int	ft_lennbr(int n)
 		len++;
 	if (n == 0)
 		len = 1;
+	len = 0;
 	while (n)
 	{
 		len++;
@@ -29,10 +30,38 @@ static int	ft_lennbr(int n)
 	return (len);
 }
 
+char	*ft_itoa(int n)
+{
+	char		*dest;
+	long int	number;
+	int			len;
+
+	number = n;
+	len = ft_lennbr(number);
+	dest = (char *)malloc(sizeof(char) * len + 1);
+	if (!dest)
+		return (NULL);
+	dest[len] = '\0';
+	if (number < 0)
+		n *= -1;
+	if (number >= 0)
+	{
+		while (len > 0)
+		{
+			dest[len - 1] = number % 10 + '0';
+			len--;
+			number = number / 10;
+		}
+	}
+	if (n < 0)
+		dest[0] = '-';
+	return (dest);
+}
+
 int	main(void)
 {
-	int	n = -145;
+	int	n = 123;
 
-	printf("%d\n", ft_lennbr(n));
+	printf("%s\n", ft_itoa(n));
 	return (0);
 }
