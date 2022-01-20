6f7b60f0bdb2d97fa8acdedbd1f4e40cf33304d8
Nathalie DE LA BURGADE <nade-la-@e1r7p15.42.fr>
2022-01-20 16:50:58 +0100

    memcpymodif

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nade-la- <nade-la-@42.fr>                  +#+  +:+       +#+        */
+/*   By: nade-la- <nade-la-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 14:28:02 by nade-la-          #+#    #+#             */
-/*   Updated: 2021/12/03 15:41:11 by nade-la-         ###   ########.fr       */
+/*   Updated: 2022/01/20 15:01:39 by nade-la-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
@@ -16,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char	*ptrdest;
 	char	*ptrsrc;
 
+	if (src == 0 || dest == 0)
+		return (0);
 	ptrsrc = (char *)src;
 	ptrdest = (char *)dest;
 	while (n-- > 0)
