1c868ef81a13c06402715bb6c39930cf45160d14
NatKW <92329992+NatKW@users.noreply.github.com>
2022-01-24 19:32:44 +0100

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: nade-la- <nade-la-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 14:28:02 by nade-la-          #+#    #+#             */
-/*   Updated: 2022/01/20 17:36:46 by nade-la-         ###   ########.fr       */
+/*   Updated: 2022/01/24 19:22:27 by nade-la-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char	*ptrdest;
 	char	*ptrsrc;
 
-	if (src == 0 || dest == 0)
+	if (src == 0 && dest == 0)
 		return (0);
 	ptrsrc = (char *)src;
 	ptrdest = (char *)dest;
-	while (n-- > 0)
+	while (n > 0)
+	{
 		*ptrdest++ = *ptrsrc++;
+		n--;
+	}	
 	return (dest);
 }
